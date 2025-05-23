979b2266b44b0e3a1d8c0341e640bf5e21f5203f
ThomasYang0318 <thomasyang0318@gmail.com>
2025-05-23 15:49:23 +0800

    refactor: reorganize chat_client.py and improve connection messages

--- chat_client.py
+++ chat_client.py
@@ -4,15 +4,31 @@ import socketio
 
 load_dotenv()
 
+import socketio
+
 sio = socketio.Client()
 
 @sio.event
 def connect():
-    print("成功連上聊天室伺服器！")
+    print("已連上聊天室伺服器！")
 
 @sio.event
 def message(data):
     print(data)
 
-server_url = os.getenv("CHAT_SERVER_URL")
-sio.connect(server_url)  
+@sio.event
+def disconnect():
+    print("與伺服器斷開連線")
+
+def send_loop():
+    while True:
+        try:
+            msg = input()
+            sio.send(msg)
+        except KeyboardInterrupt:
+            break
+
+if __name__ == '__main__':
+    server_url = os.getenv("CHAT_SERVER_URL")
+    sio.connect(server_url) 
+    send_loop()
