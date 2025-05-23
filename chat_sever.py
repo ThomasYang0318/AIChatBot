6bd367bdeade8c22d773eda4a246462e8229b116
ThomasYang0318 <thomasyang0318@gmail.com>
2025-05-23 13:39:57 +0800

    Add server + requirements + Procfile

---
+++ chat_sever.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chat-secret'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"[Server 收到] {msg}")
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
